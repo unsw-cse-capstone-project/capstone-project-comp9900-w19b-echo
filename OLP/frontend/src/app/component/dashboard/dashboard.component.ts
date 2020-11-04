3358085f445ea5e01b595c3f36a777779734427f
Jacky Yang <yang.wenxue@gmail.com>
2020-11-04 17:16:48 +1100

    Add search to Dashboard

--- OLP/frontend/src/app/component/dashboard/dashboard.component.ts
+++ OLP/frontend/src/app/component/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {Property} from "../../model/property.model";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {NbToastrService} from "@nebular/theme";
+import {UserService} from "../../service/user.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +12,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  isLoading: boolean = false;
+  properties: Property[] = [];
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient, private toastrService: NbToastrService, private userService: UserService) {
+  }
 
   ngOnInit(): void {
   }
 
+  searchProperty($event: any) {
+    this.isLoading = true;
+    let uri = $event ? '/search-property-like' : '/listAllProperty';
+    this.http.post(environment.baseEndpoint + '/search-property-like', {keyword: $event})
+      .subscribe( (p : Property[])=> {
+          this.properties = p;
+          this.isLoading = false;
+        }
+      );
+  }
 }
