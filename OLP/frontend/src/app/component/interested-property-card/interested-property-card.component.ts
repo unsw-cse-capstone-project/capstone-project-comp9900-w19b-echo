706bf3e56500c5ed7cbe5fb547a7b9919c2c3dff
Jacky Yang <yang.wenxue@gmail.com>
2020-11-06 21:30:10 +1100

    Integrate backend API for dashboard

--- OLP/frontend/src/app/component/interested-property-card/interested-property-card.component.ts
+++ OLP/frontend/src/app/component/interested-property-card/interested-property-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {PropertyAuction} from "../../model/property-auction.model";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-interested-property-card',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interested-property-card.component.scss']
 })
 export class InterestedPropertyCardComponent implements OnInit {
+  properties: PropertyAuction[] = [];
+  isLoading: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private http: HttpClient) {
   }
 
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.http.post(environment.baseEndpoint + '/view-favorite', {})
+      .subscribe( (data : PropertyAuction[])=> {
+          this.properties = data;
+          this.isLoading = false;
+        }
+      );
+  }
+
+  getCount(status : number[]) {
+    return this.properties.filter(p => status.includes(p.property.status)).length;
+  }
 }
