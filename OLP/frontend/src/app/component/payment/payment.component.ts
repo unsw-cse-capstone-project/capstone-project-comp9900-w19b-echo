721b4bb9652883e5f9da0857a6370dc80efaed4e
Jacky Yang <yang.wenxue@gmail.com>
2020-11-06 15:28:23 +1100

    Add user address, payment update

--- OLP/frontend/src/app/component/payment/payment.component.ts
+++ OLP/frontend/src/app/component/payment/payment.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {UserService} from "../../service/user.service";
 import {NbToastrService} from "@nebular/theme";
@@ -13,43 +13,19 @@ import {NbComponentStatus} from "@nebular/theme/components/component-status";
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-  paymentDetail: PaymentDetail = new PaymentDetail();
   isLoading: boolean = false;
   @Input() user: User;
+  @Input() paymentDetail: PaymentDetail;
+  @Output() save: EventEmitter<any> = new EventEmitter();
 
   constructor(private http: HttpClient, private userService: UserService, private toastrService: NbToastrService) {
   }
 
   ngOnInit(): void {
-    this.getPaymentDetail();
   }
 
-  getPaymentDetail() {
-    const uid = this.userService.user?.uid;
-    if(uid) {
-      this.isLoading = true;
-      this.http.post(environment.baseEndpoint + '/view-payment', {uid: uid})
-        .subscribe((paymentDetails: PaymentDetail[]) => {
-            if (paymentDetails && paymentDetails.length > 0){
-              this.paymentDetail = paymentDetails[0];
-            }
-            this.isLoading = false;
-          }
-        );
-    }
-  }
-
-  save() {
-    this.isLoading = true;
-    let uri = this.paymentDetail.serial ? '/update-payment' : '/add-payment';
-    this.paymentDetail.uid = this.userService.currentUser.uid;
-    this.http.post(environment.baseEndpoint + uri, {paymentDetail: this.paymentDetail})
-      .subscribe((u: User) => {
-          this.showToast('success');
-          this.getPaymentDetail();
-          this.isLoading = false;
-        }
-      );
+  savePayment() {
+    this.save.emit(this.paymentDetail);
   }
 
   showToast(status: NbComponentStatus) {
